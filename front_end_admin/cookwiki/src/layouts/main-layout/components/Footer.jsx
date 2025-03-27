2c45f22b125f017892344769c320f8b15b3db825
anvd88 <duyanvu2409@gmail.com>
2025-03-27 09:32:35 +0700

    update
    up

--- front_end_admin/cookwiki/src/layouts/main-layout/components/Footer.jsx
+++ front_end_admin/cookwiki/src/layouts/main-layout/components/Footer.jsx
@@ -3,7 +3,7 @@ import {cn} from "@/lib/utils";
 function Footer() {
     return (
         <footer id="footer" className={cn("p-2")}>
-            <p className={cn("text-sm text-gray-600 text-center")}>Bản quyền của © Cookpad Inc. All Rights Reserved</p>
+            <p className={cn("text-sm text-gray-600 text-center")}> All Rights Reserved</p>
         </footer>
     )
 }
